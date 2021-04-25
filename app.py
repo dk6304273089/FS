d7b6b4d4dfdeb23237786407552a8e77e6542ee0
unknown <dk6304273089@gmail.com>
2021-04-25 11:04:42 +0530

    updated heroku version 9.99990

--- app.py
+++ app.py
@@ -7,21 +7,34 @@ from pywebio import start_server
 import pickle
 import argparse
 model = pickle.load(open('classifier.pkl', 'rb'))
+app = Flask(__name__)
 
-age=input("enter the value of age: ",type=NUMBER)
-sex=radio("CHOOSE THE GENDER: ",options=[0,1])
-cp=input("enter the cp: ",type=NUMBER)
-trestbps=input("enter the trestbps: ",type=NUMBER)
-chol=input("enter the chol: ",type=NUMBER)
-fbs=input("enter the fbs: ",type=NUMBER)
-restecg=input("enter the restecg: ",type=NUMBER)
-thalach=input("enter the thalach: ",type=NUMBER)
-exang=input("enter the exang: ",type=NUMBER)
-oldpeak=input("enter the value of oldpeak: ",type=FLOAT)
-slope=input("enter the value of slope: ",type=NUMBER)
-ca=input("enter the ca: ",type=NUMBER)
-thal=input("enter the value of thal: ",type=NUMBER)
+def predict():
+    age=input("enter the value of age: ",type=NUMBER)
+    sex=radio("CHOOSE THE GENDER: ",options=[0,1])
+    cp=input("enter the cp: ",type=NUMBER)
+    trestbps=input("enter the trestbps: ",type=NUMBER)
+    chol=input("enter the chol: ",type=NUMBER)
+    fbs=input("enter the fbs: ",type=NUMBER)
+    restecg=input("enter the restecg: ",type=NUMBER)
+    thalach=input("enter the thalach: ",type=NUMBER)
+    exang=input("enter the exang: ",type=NUMBER)
+    oldpeak=input("enter the value of oldpeak: ",type=FLOAT)
+    slope=input("enter the value of slope: ",type=NUMBER)
+    ca=input("enter the ca: ",type=NUMBER)
+    thal=input("enter the value of thal: ",type=NUMBER)
 
-prediction=model.predict([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
-output=prediction[0]
-put_text("the target value is",output)
+    prediction=model.predict([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+    output=prediction[0]
+    put_text("the target value is",output)
+
+app.add_url_rule('/tool', 'webio_view', webio_view(predict),
+            methods=['GET', 'POST', 'OPTIONS'])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    args = parser.parse_args()
+
+    start_server(predict, port=args.port)
