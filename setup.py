504e484252898dbe547e12853de9f496587558bf
unknown <dk6304273089@gmail.com>
2021-04-26 14:43:13 +0530

    second

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    description="its a wine Q package",
+    description="its a heartdisease",
     author="DHEERAJ",
     packages=find_packages(),
     license="MIT"
